e788a4d64a6153d14bfbce6fbddfc8a44ba562d8
Hari Balakrishnan <harish375@gmail.com>
2015-12-15 23:56:55 -0800

    [spring mvc commit]

---
+++ src/main/java/com/samples/springmvc/portal/configuration/SameOriginPolicyFilter.java
@@ -0,0 +1,27 @@
+package com.samples.springmvc.portal.configuration;
+
+import javax.servlet.Filter;
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class SameOriginPolicyFilter implements Filter {
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Filtering on...........................................................");
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
+        chain.doFilter(req, res);
+    }
+ 
+    public void init(FilterConfig filterConfig) {}
+ 
+    public void destroy() {}
+}
